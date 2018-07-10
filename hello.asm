21df6f3e3eb8c934c2f2686b12eb130b876f060f
takadev <nontan20xx@gmail.com>
2018-07-10 18:49:13 +0900

    Fix hello

--- hello.asm
+++ hello.asm
@@ -6,8 +6,12 @@ message: db 'Hello, world!!', 10
 section .text
 
 _start:
-    mov rax, 1 ; system call number
+    mov rax, 1 ; system call write
     mov rdi, 1 ; output descriptor
     mov rsi, message ; output message pointer
     mov rdx, 14
+    syscall
+
+    mov rax, 60 ; system call exit
+    xor rdi, rdi
     syscall
